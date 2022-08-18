7f951f8168e8cd342318d4f969b488ece0cc6482
Kotty <kotty@guns-n-girls.de>
2022-08-18 09:22:07 +0200

    role must be inverter

--- dbus-growatt-shinex.py
+++ dbus-growatt-shinex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
- 
+
 # import normal packages
-import platform 
+import platform
 import logging
 import sys
 import os
@@ -14,11 +14,10 @@ import sys
 import time
 import requests # for http GET
 import configparser # for config/ini file
- 
+
 # our own packages from victron
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
 from vedbus import VeDbusService
-
 
 class DbusGrowattShineXService:
   def __init__(self, servicename, paths, productname='Growatt ShineX', connection='Growatt ShineX HTTP Json Connection'):
@@ -26,33 +25,33 @@ class DbusGrowattShineXService:
     deviceinstance = int(config['DEFAULT']['Deviceinstance'])
     customname = config['DEFAULT']['CustomName']
     phase = config['DEFAULT']['Phase']
-    
+
     self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance))
     self._paths = paths
-    
+
     logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
-    
+
     # Create the management objects, as specified in the ccgx dbus-api document
     self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
     self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
     self._dbusservice.add_path('/Mgmt/Connection', connection)
-    
+
     # Create the mandatory objects
     self._dbusservice.add_path('/DeviceInstance', deviceinstance)
     self._dbusservice.add_path('/ProductId', 0xB099) # id needs to be assigned by Victron Support current value for testing
     self._dbusservice.add_path('/DeviceType', 666) # found on https://www.sascha-curth.de/projekte/005_Color_Control_GX.html#experiment - should be an ET340 Engerie Meter
     self._dbusservice.add_path('/ProductName', productname)
-    self._dbusservice.add_path('/CustomName', customname)    
-    self._dbusservice.add_path('/Latency', None)    
+    self._dbusservice.add_path('/CustomName', customname)
+    self._dbusservice.add_path('/Latency', None)
     self._dbusservice.add_path('/FirmwareVersion', 0.1)
     self._dbusservice.add_path('/HardwareVersion', 0)
     self._dbusservice.add_path('/Connected', 1)
-    self._dbusservice.add_path('/Role', 'grid')
+    self._dbusservice.add_path('/Role', 'pvinverter')
     self._dbusservice.add_path('/Position', 0) # normaly only needed for pvinverter
     #self._dbusservice.add_path('/Serial', self._getShineXSerial())
     self._dbusservice.add_path('/Serial', '0815')
     self._dbusservice.add_path('/UpdateIndex', 0)
-    
+
     # add path values to dbus
     for path, settings in self._paths.items():
       self._dbusservice.add_path(
@@ -63,100 +62,100 @@ class DbusGrowattShineXService:
 
     # add _update function 'timer'
     gobject.timeout_add(250, self._update) # pause 250ms before the next request
-    
+
     # add _signOfLife 'timer' to get feedback in log every 5minutes
     gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
- 
+
   def _getShineXSerial(self):
-    meter_data = self._getShineXData()  
-    
+    meter_data = self._getShineXData()
+
     if not meter_data['mac']:
         raise ValueError("Response does not contain 'mac' attribute")
-    
+
     serial = meter_data['mac']
     return serial
- 
- 
+
+
   def _getConfig(self):
     config = configparser.ConfigParser()
     config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
     return config;
- 
- 
+
+
   def _getSignOfLifeInterval(self):
     config = self._getConfig()
     value = config['DEFAULT']['SignOfLifeLog']
-    
-    if not value: 
+
+    if not value:
         value = 0
-    
+
     return int(value)
-  
-  
+
+
   def _getShineXStatusUrl(self):
     config = self._getConfig()
     accessType = config['DEFAULT']['AccessType']
-    
-    if accessType == 'OnPremise': 
+
+    if accessType == 'OnPremise':
         URL = "http://%s:%s@%s/status" % (config['ONPREMISE']['Username'], config['ONPREMISE']['Password'], config['ONPREMISE']['Host'])
         URL = URL.replace(":@", "")
     else:
         raise ValueError("AccessType %s is not supported" % (config['DEFAULT']['AccessType']))
-    
+
     return URL
-    
- 
+
+
   def _getShineXData(self):
     URL = self._getShineXStatusUrl()
     meter_r = requests.get(url = URL)
-    
+
     # check for response
     if not meter_r:
         raise ConnectionError("No response from Shine X - %s" % (URL))
-    
-    meter_data = meter_r.json()     
-    
+
+    meter_data = meter_r.json()
+
     # check for Json
     if not meter_data:
         raise ValueError("Converting response to JSON failed")
-    
+
     if meter_data['Status'] == "Disconnected":
         logging.info("Stick not connected to Inverter")
         meter_data='{"Status": "Normal","DcVoltage": 0,"AcFreq": 50.000,"AcVoltage": 239.5,"AcPower": 0,"EnergyToday": 0,"OperatingTime": 0,"Temperature": 0,"AccumulatedEnergy": 0, "Cnt": 0}'.json()
-    
+
     return meter_data
- 
- 
+
+
   def _signOfLife(self):
     logging.info("--- Start: sign of life ---")
     logging.info("Last _update() call: %s" % (self._lastUpdate))
     logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
     logging.info("--- End: sign of life ---")
     return True
- 
-  def _update(self):   
+
+  def _update(self):
     try:
        #get data from Shine X
        meter_data = self._getShineXData()
-      
-      
-       
+
+
+
        #send data to DBus
        self._dbusservice['/Ac/' + phase + '/Voltage'] = meter_data['AcVoltage']
- 
+
        current = meter_data['AcPower'] / meter_data['AcVoltage']
        self._dbusservice['/Ac/' + phase + '/Current'] = current
-       
+
        self._dbusservice['/Ac/' + phase + '/Power'] = meter_data['AcPower']
        self._dbusservice['/Ac/' + phase + '/Energy/Forward'] = meter_data['EnergyTotal']
-        
+
        self._dbusservice['/Ac/Energy/Forward'] = self._dbusservice['/Ac/' + phase + '/Energy/Forward']
-          
+
        #logging
        logging.debug("House Consumption (/Ac/Power): %s" % (self._dbusservice['/Ac/Power']))
        logging.debug("House Forward (/Ac/Energy/Forward): %s" % (self._dbusservice['/Ac/Energy/Forward']))
        logging.debug("---");
-       
+
        # increment UpdateIndex - to show that new data is available
        index = self._dbusservice['/UpdateIndex'] + 1  # increment index
        if index > 255:   # maximum value of the index
@@ -164,17 +163,17 @@ class DbusGrowattShineXService:
        self._dbusservice['/UpdateIndex'] = index
 
        #update lastupdate vars
-       self._lastUpdate = time.time()              
+       self._lastUpdate = time.time()
     except Exception as e:
        logging.critical('Error at %s', '_update', exc_info=e)
-       
+
     # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
     return True
- 
+
   def _handlechangedvalue(self, path, value):
     logging.debug("someone else updated %s to %s" % (path, value))
     return True # accept the change
- 
+
 
 
 def main():
@@ -186,39 +185,39 @@ def main():
                                 logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
                                 logging.StreamHandler()
                             ])
- 
+
   try:
       logging.info("Start");
-  
+
       from dbus.mainloop.glib import DBusGMainLoop
       # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
       DBusGMainLoop(set_as_default=True)
-     
-      #formatting 
+
+      #formatting
       _kwh = lambda p, v: (str(round(v, 2)) + 'KWh')
       _a = lambda p, v: (str(round(v, 1)) + 'A')
       _w = lambda p, v: (str(round(v, 1)) + 'W')
-      _v = lambda p, v: (str(round(v, 1)) + 'V')   
-     
+      _v = lambda p, v: (str(round(v, 1)) + 'V')
+
       #start our main-service
       pvac_output = DbusGrowattShineXService(
         servicename='com.victronenergy.grid',
         paths={
           '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh}, # energy bought from the grid
           '/Ac/Power': {'initial': 0, 'textformat': _w},
-          
+
           '/Ac/Current': {'initial': 0, 'textformat': _a},
           '/Ac/Voltage': {'initial': 0, 'textformat': _v},
-          
+
           '/Ac/L1/Voltage': {'initial': 0, 'textformat': _v},
           '/Ac/L1/Current': {'initial': 0, 'textformat': _a},
           '/Ac/L1/Power': {'initial': 0, 'textformat': _w},
           '/Ac/L1/Energy/Forward': {'initial': 0, 'textformat': _kwh},
         })
-     
+
       logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
       mainloop = gobject.MainLoop()
-      mainloop.run()            
+      mainloop.run()
   except Exception as e:
     logging.critical('Error at %s', 'main', exc_info=e)
 if __name__ == "__main__":
