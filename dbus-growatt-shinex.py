17b1a47d6f535d3da8d3d93c583fd28de55c0655
Kotty <kotty@guns-n-girls.de>
2024-05-23 11:51:05 +0200

    fixes

--- dbus-growatt-shinex.py
+++ dbus-growatt-shinex.py
@@ -130,6 +130,8 @@ class DbusGrowattShineXService:
 
     try:
       meter_data = meter_r.json()
+    except:
+        logging.info("Got no Json. meter_data set to: %s" % (meter_data))
 
     return meter_data
 
@@ -147,7 +149,6 @@ class DbusGrowattShineXService:
       config = self._getConfig()
       phase = config['DEFAULT']['Phase']
       #get data from Shine X
-      print(meter_data)
 
       #send data to DBus
       meter_data = self._getShineXData()
@@ -229,7 +230,7 @@ class DbusGrowattShineXService:
 
 def main():
   logpath = '/var/log/%s' % (os.path.dirname(os.path.realpath(__file__)).split('/')[-1])
-  if not os.path.isdir(logpath):
+  if not os.path.exists(logpath):
       os.mkdir(logpath,0o755)
   #configure logging
   log_rotate_handler = logging.handlers.RotatingFileHandler(
@@ -237,8 +238,8 @@ def main():
       backupCount=2,
       encoding=None,
       delay=0,
-      filename="%s/current.log" % (logpath)
-  logging.basicConfig(      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+      filename="%s/current.log" % (logpath))
+  logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
       datefmt='%Y-%m-%d %H:%M:%S',
       level=logging.INFO,
       handlers=[
